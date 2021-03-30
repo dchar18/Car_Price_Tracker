71fb1d65e881cf6596f88ffecf598d9aae586fca
Damian Charczuk <dcharapple@icloud.com>
2021-03-29 23:52:07 -0500

    Entries through 3/29

--- car_price_all.py
+++ car_price_all.py
@@ -9,11 +9,11 @@ import sys
 import datetime
 
 cars = [
-    ('lexus', 'gsf', '60004'),
-    ('lexus', 'rcf', '60004'),
-    ('lexus', 'lc500', '60004'),
-    ('lexus', 'gs350', '60004', 'F Sport'),
-    ('lexus', 'is350', '60004', 'F Sport'),
+    # ('lexus', 'gsf', '60004'),
+    # ('lexus', 'rcf', '60004'),
+    # ('lexus', 'lc500', '60004'),
+    # ('lexus', 'gs350', '60004', 'F Sport'),
+    # ('lexus', 'is350', '60004', 'F Sport'),
     ('mazda', 'mazdaspeed3', '60004'),
     ('tesla', 'model3', '60004'),
     # ('toyota', 'rav4', '60004'),
